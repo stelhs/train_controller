897fa26e5dfbf5bf7673706ddf274999658ab685
Michail Kurockin <Michail.Kurachkin@promwad.com>
2016-07-09 18:00:45 +0300

    added temporary test app

--- train_controller.c
+++ train_controller.c
@@ -6,9 +6,10 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include "train_controller.h"
+#include "board.h"
 #include "types.h"
-
 
 void gerkon_state_changed(void *arg)
 {
@@ -18,16 +19,20 @@ void gerkon_state_changed(void *arg)
 void traction_up_handler(void *arg)
 {
 	printf("traction_up\r\n");
+	ac_motor_enable_bacward(&motor_left);
 }
 
 void traction_down_handler(void *arg)
 {
 	printf("traction_down\r\n");
+	ac_motor_enable_forward(&motor_left);
 }
 
 void traction_reset_handler(void *arg)
 {
 	printf("traction_reset\r\n");
+	ac_motor_disable(&motor_left);
+
 }
 
 void traction_reverse_handler(void *arg, t_counter hold_counter)
@@ -37,5 +42,8 @@ void traction_reverse_handler(void *arg, t_counter hold_counter)
 
 void power_balance_regulator_changed(s16 value)
 {
-	printf("balance = %d\r\n", value);
+	printf("bal=%d\r\n", value);
+	printf("power=%d\r\n", abs(value));
+	ac_motor_set_power(&motor_left, abs(value));
+	ac_motor_set_power(&motor_right, abs(value));
 }
