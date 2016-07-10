af7c36d3c2811320aaca06da9b71b3cb53ecb3b0
Michail Kurockin <Michail.Kurachkin@promwad.com>
2016-07-10 17:02:16 +0300

    fixed speedometer

--- speedometer.c
+++ speedometer.c
@@ -8,16 +8,34 @@
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include "list.h"
+#include "sys_timer.h"
+#include "speedometer.h"
 #include "gpio.h"
 
 #define SPEED_INDICATOR_MAX 40 /* max speed value Km/h */
 #define SPEED_INDICATOR_PWM_MAX 235 /* max PWD output value */
 
+void speedometer_timer(void *arg);
+static struct speedometer sm = {
+	.timer = {
+		.devisor = 100,
+		.handler = speedometer_timer,
+		.priv = &sm,
+	},
+	.speed = 0,
+};
 
 /* input tahogenerator signal */
 ISR(INT1_vect)
 {
-	printf("x\r\n");
+	sm.taho_counter ++;
+}
+
+
+void speedometer_timer(void *arg)
+{
+	sm.speed = sm.taho_counter;
+	sm.taho_counter = 0;
 }
 
 /**
@@ -32,12 +50,23 @@ void speed_indicator_set(u8 speed)
 	OCR0 = ((u32)speed * SPEED_INDICATOR_PWM_MAX) / SPEED_INDICATOR_MAX;
 }
 
+/**
+ * Return current speed
+ * @return
+ */
+u8 speedometer_get_speed(void)
+{
+	return sm.speed;
+}
+
 
 void speedometer_init(void)
 {
 	/* configure external interrupts for Int1 */
 	MCUCR |= _BV(ISC10);
 	GICR |= _BV(INT1);
+
+	sys_timer_add_handler(&sm.timer);
 }
 
 
--- speedometer.h
+++ speedometer.h
@@ -7,7 +7,17 @@
 #ifndef SPEEDOMETER_H_
 #define SPEEDOMETER_H_
 
+#include "types.h"
+#include "sys_timer.h"
+
+struct speedometer {
+	struct sys_timer timer;
+	u16 speed;
+	u16 taho_counter;
+};
+
 void speedometer_init(void);
 void speed_indicator_set(u8 speed);
+u8 speedometer_get_speed(void);
 
 #endif /* SPEEDOMETER_H_ */
